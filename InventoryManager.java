b9bb4b749434403891be45876cdbe390cc2e5dd3
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-09-23 16:49:52 +0400

    Question 1 is Done

---
+++ InventoryManager.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class InventoryManager {
+    private Map<String, Integer> inventory = new HashMap<>();
+
+    public InventoryManager() {
+        inventory.put("Apples", 50);
+        inventory.put("Oranges", 30);
+    }
+
+    /**
+     * TO-DO: Add 'quantityToAdd' to the stock of 'itemName'.
+     * If 'itemName' doesn't exist, add it to the map with that quantity.
+     \*
+     * @param itemName The name of the item to restock.
+     * @param quantityToAdd The number of items to add.
+     */
+    public void restockItem(String itemName, int quantityToAdd) {
+        // Hint: Use getOrDefault() to simplify your code.
+        // Your code here
+        int currentQuantity= inventory.getOrDefault(itemName,0);
+        inventory.put(itemName,currentQuantity+quantityToAdd);
+
+
+
+    }
+
+    public Map<String, Integer> getInventory() {
+        return inventory;
+    }
+}
