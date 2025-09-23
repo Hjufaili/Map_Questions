d737e8e242b40fe888501a70b91936a07c54de45
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-09-23 16:51:45 +0400

    Question 5 is Done

---
+++ PhoneBook.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class PhoneBook {
+    private Map<String, String> contacts = new HashMap<>();
+
+    public PhoneBook() {
+        contacts.put("Alice", "555-1111");
+        contacts.put("Bob", "555-2222");
+    }
+
+    /**
+     * TO-DO: Remove the contact only if the name and number match.
+     \*
+     * @param name The name of the contact to remove.
+     * @param number The phone number that must match for the removal to happen.
+     * @return true if the contact was removed, false otherwise.
+     */
+    public boolean removeContact(String name, String number) {
+        // Your code here. Use the remove(key, value) method.
+        if (number.equals(contacts.get(name))) {
+            contacts.remove(name);
+            return true;
+        }
+        return false; // Placeholder
+
+       // return contacts.remove(name, number);
+
+    }
+
+
+
+    public Map<String, String> getContacts() {
+        return contacts;
+    }
+}
